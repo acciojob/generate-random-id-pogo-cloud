4792df9e543e0b99369a77a8d1d40183df9d2681
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-06-27 21:49:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	let character = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let str = "";
+    for (let i = 0; i < l; i++) {
+        let index = Math.floor(Math.random() * character.length);
+        str += character[index];
+    }
+    return str;
 }
 
 // Do not change the code below.
